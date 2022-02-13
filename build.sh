8845d1e3dd21f670892839ebd4dacc8193d027f5
agmo1993 <abdulrehmanm93@gmail.com>
2022-02-13 19:32:24 +1100

    build and install files split

--- build.sh
+++ build.sh
@@ -22,42 +22,9 @@ do
  --security-group-ids ${SG}  
      
    BROKER_IPS+=("10.0.2.1${COUNTER}")
-   echo "zoo${COUNTER} 10.0.2.1${COUNTER}" >> configuration/hosts
+   echo "10.0.2.1${COUNTER} zoo${COUNTER}" >> configuration/hosts
    echo "server.${COUNTER}=zoo${COUNTER}:2888:3888" >> configuration/zookeeper.properties
    let COUNTER=COUNTER+1 
 done
 
-# Configure the Kafka brokers, install openjdk and confluence community
-
-INDEX=1
-for IP in "${BROKER_IPS[@]}"
-do
-    # Install openjdk and confluent-community
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "wget -qO - https://packages.confluent.io/deb/5.2/archive.key | sudo apt-key add -"
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo add-apt-repository 'deb [arch=amd64] https://packages.confluent.io/deb/5.2 stable main'"
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo apt-get update && sudo apt-get install -y openjdk-8-jdk confluent-community-${C_VERSION}"
-
-    # Configure Zookeeper and Kafka
-    scp -i ${KEY_LOCATION} ./configuration/hosts ubuntu@${IP}:/home/ubuntu
-    scp -i ${KEY_LOCATION} ./configuration/zookeeper.properties ubuntu@${IP}:/home/ubuntu
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./hosts /etc" 
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./zookeeper.properties /etc/kafka"
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "echo "${INDEX}" | sudo tee -a /var/lib/zookeeper/myid"
-    sed "s/BrokerID/${INDEX}/g" ./configuration/serverSample.properties > ./configuration/server.properties
-    scp -i ${KEY_LOCATION} ./configuration/server.properties ubuntu@${IP}:/home/ubuntu
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./hosts /etc/kafka" 
-    let INDEX=INDEX+1 
-done
-
-# Start the zookeeper instances, once the configurations have been set
-for IP in "${BROKER_IPS[@]}"
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo systemctl start confluent-zookeeper; sudo systemctl enable confluent-zookeeper"
-do
-
-# Start Kafka instances on each broker, after providing some time for zookeeper to start
-sleep 10
-
-# Start the zookeeper instances, once the configurations have been set
-for IP in "${BROKER_IPS[@]}"
-    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo systemctl start confluent-kafka; sudo systemctl enable confluent-kafka"
-do
+echo "Broker IPs are ${BROKER_IPS[*]}"
---
+++ installCluster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Configure the Kafka brokers, install openjdk and confluence community
+export BROKER_IPS=(10.0.2.11 10.0.2.12 10.0.2.13)
+echo "${BROKER_IPS[*]}"
+
+INDEX=1
+for IP in "${BROKER_IPS[@]}"
+do
+    # Install openjdk and confluent-community
+    echo "Configure broker at ${IP}"
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} -o "StrictHostKeyChecking=no" "wget -qO - https://packages.confluent.io/deb/5.2/archive.key | sudo apt-key add -"
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} -o "StrictHostKeyChecking=no" "sudo add-apt-repository 'deb [arch=amd64] https://packages.confluent.io/deb/5.2 stable main'"
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} -o "StrictHostKeyChecking=no" "sudo apt-get update && sudo apt-get install -y openjdk-8-jdk confluent-community-${C_VERSION}"
+
+    # Configure Zookeeper and Kafka
+    scp -i ${KEY_LOCATION} ./configuration/hosts ubuntu@${IP}:/home/ubuntu
+    scp -i ${KEY_LOCATION} ./configuration/zookeeper.properties ubuntu@${IP}:/home/ubuntu
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./hosts /etc" 
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./zookeeper.properties /etc/kafka"
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} "echo "${INDEX}" | sudo tee -a /var/lib/zookeeper/myid"
+    sed "s/BrokerID/${INDEX}/g" ./configuration/serverSample.properties > ./configuration/server.properties
+    scp -i ${KEY_LOCATION} ./configuration/server.properties ubuntu@${IP}:/home/ubuntu
+    ssh ubuntu@${IP} -i ${KEY_LOCATION} "sudo mv ./server.properties /etc/kafka" 
+    let INDEX=INDEX+1 
+done
+
+# Start the zookeeper instances, once the configurations have been set
+for IP_ADD in "${BROKER_IPS[@]}"
+do
+    ssh ubuntu@${IP_ADD} -i ${KEY_LOCATION} "sudo systemctl start confluent-zookeeper; sudo systemctl enable confluent-zookeeper"
+    sleep 5
+done
+
+# Start Kafka instances on each broker, after providing some time for zookeeper to start
+sleep 15
+
+# Start the zookeeper instances, once the configurations have been set
+for IP_ADD in "${BROKER_IPS[@]}"
+do
+    ssh ubuntu@${IP_ADD} -i ${KEY_LOCATION} "sudo systemctl start confluent-kafka; sudo systemctl enable confluent-kafka"
+    sleep 5
+done
